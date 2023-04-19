553af88a3fd0820cf012a0af88a4135fe1bdff46
Aman Satyawali <amansatyawali@gmail.com>
2023-04-20 00:39:18 +0530

    bug fix in merge sort

--- dsa/sorting/merge.py
+++ dsa/sorting/merge.py
@@ -13,13 +13,11 @@ def merge(arr, first , mid, last ) :
         mid : index of the start of the second sub array (index of the last element of first sub array should be mid-1)
         last : index of the last element of the second sub array
     """
-    print('arr', arr, first, mid, last)
-    aux = arr[first : last + 1]
+    aux = arr.copy()
     i = first
     j = mid
     k = first
     while True:
-        (print(k, i, j))
         if i == mid :           #Check if all the elements of the first sub array have been added to the main array
             arr[k] = aux[j]
             j += 1
@@ -40,9 +38,9 @@ def merge(arr, first , mid, last ) :
 
 if __name__ == "__main__" :
     
-    # arr = [2, 4, 6, 8] + [1, 3, 5, 7]
-    arr = [x for x in range(1, 20, 3)] + [x for x in range(2, 20, 3)]
+    arr = [4] + [3]
+    # arr = [x for x in range(1, 20, 3)] + [x for x in range(2, 20, 3)]
     print(arr)
-    merge(arr, first = 0, mid = 7, last = 12)
+    merge(arr, first = 0, mid = 1, last = 1)
     print(arr)
     print(is_sorted(arr, len(arr)))
--- dsa/sorting/merge_sort.py
+++ dsa/sorting/merge_sort.py
@@ -3,26 +3,24 @@ from utils import is_sorted, create_random_int_array, less
 from merge import merge
 
 def merge_sort(arr, first, last) :
-    # print('beginning first, last -> ', first, last)
     if first == last :
-        # print('returning ', [arr[first]])
         return [arr[first]]
     else :
         mid = first + (last - first) // 2
-        # print('recalling - > ', first, mid, last)
-        time.sleep(2)
         merge_sort(arr, first, mid)
         merge_sort(arr, mid + 1, last)
         res = merge(arr, first, mid  + 1, last)
-        # print('returning', res)
         return res
 
 
 if __name__ == "__main__" :
-    ARRAY_SIZE = 4
+    ARRAY_SIZE = 10000
     arr = create_random_int_array(ARRAY_SIZE)
-    print(arr)
+    # arr = [4, 3, 2, 1]
+    start_time = time.time()
     merge_sort(arr, 0, ARRAY_SIZE - 1)
-    print(arr)
+    end_time = time.time()
+
+    print('time take = ', end_time - start_time)
 
     print(is_sorted(arr, ARRAY_SIZE))
