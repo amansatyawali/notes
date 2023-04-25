3ad5ea471e7d492f8ae2697ef4c1144106c193f2
Aman Satyawali <amansatyawali@gmail.com>
2023-04-25 22:16:57 +0530

    fixed, structure

--- dsa/sorting/merge_sort_without_recursion.py
+++ dsa/sorting/merge_sort_without_recursion.py
@@ -5,14 +5,14 @@ from merge import merge
 def merge_sort(arr, n) :
     if(n < 2) :
         return arr
-    sub_array_size = 2
+    sub_array_size = 2                                 #This sub array is the combined array, ie including the 2 arrays to be merged
     while sub_array_size < n * 2 :
         i = 0
         while i < n :                                   #Making the subarrays one bye one
             first = i
             last = i + sub_array_size - 1
             mid = first + (last - first) // 2
-            if mid >= n - 1:                               #If last few elements are left (which we know are sorted in previous iterations), we will not merge that part this time
+            if mid >= n - 1:                            #If last few elements are left (which we know are sorted in previous iterations), we will not merge that part this time
                 break                                   #probably, next time it will be merged, but it will be definitely be merged by the end.
             if last >= n :
                 last = n - 1
