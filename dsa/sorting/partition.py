f1e164f3ebfc0fd2a136fc1391eea9fa030c2e56
Aman Satyawali <amansatyawali@gmail.com>
2023-05-10 21:06:44 +0530

    added partition.py and quick soer

---
+++ dsa/sorting/partition.py
@@ -0,0 +1,13 @@
+from utils import is_sorted, create_random_int_array, less
+
+def partition(arr, first , mid, last ) :
+
+
+
+if __name__ == "__main__" :
+    
+    arr = [4, 3]
+    print(arr)
+    partition(arr, first = 0, mid = 1, last = 1)
+    print(arr)
+    print(is_sorted(arr, len(arr)))
